c7e96a4c4ebf1495accb14b89fa337b959701b7f
KiLAR <dpaul3333@gmail.com>
2024-05-09 14:29:54 +0530

    back button and timer

--- Assets/Scripts/UI/UImanager.cs
+++ Assets/Scripts/UI/UImanager.cs
@@ -5,9 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class UImanager : MonoBehaviour
 {
+    public GameObject settingspanel;
     public void BackToMenuScene()
     {
         Debug.Log("Loading Menu");
         SceneManager.LoadScene(0);
+    }
+    public void SettingsButton()
+    {
+        settingspanel.SetActive(true);
     }
 }
